df15ee0c4d86a530d046a9183fd2b0d1874e44ca
tonylib <tonylib@gmail.com>
2013-11-07 11:28:39 -0500

    Use ssl if protocol is https

--- src/main/java/com/nxttxn/axis2/transport/vertx/VertxSender.java
+++ src/main/java/com/nxttxn/axis2/transport/vertx/VertxSender.java
@@ -6,7 +6,6 @@ import org.apache.axis2.Constants;
 import org.apache.axis2.addressing.RelatesTo;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.MessageContext;
-import org.apache.axis2.description.Parameter;
 import org.apache.axis2.description.TransportOutDescription;
 import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.axis2.engine.MessageReceiver;
@@ -20,20 +19,20 @@ import org.apache.commons.io.output.WriterOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.Vertx;
 import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpClient;
 import org.vertx.java.core.http.HttpClientRequest;
 import org.vertx.java.core.http.HttpClientResponse;
 import org.vertx.java.core.http.HttpServerResponse;
-import org.vertx.java.deploy.impl.VertxLocator;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.UnsupportedCharsetException;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
@@ -47,6 +46,7 @@ public class VertxSender extends AbstractTransportSender {
     private Logger log = LoggerFactory.getLogger(VertxSender.class);
 
     private VertxConnectionFactoryManager connectionFactoryManager;
+    private Hashtable<String, String> properties = new Hashtable<String, String>();
 
     @Override
     public void init(ConfigurationContext cfgCtx, TransportOutDescription transportOutDescription) throws AxisFault {
@@ -56,25 +56,101 @@ public class VertxSender extends AbstractTransportSender {
     }
 
     @Override
-    public void sendMessage(final MessageContext messageContext, final String targetEPR, OutTransportInfo outTransportInfo) throws AxisFault {
+    public void sendMessage(final MessageContext messageContext, String targetEPR, OutTransportInfo outTransportInfo) throws AxisFault {
         if (messageContext.getProperty(HttpAxis2ServerHandler.RESPONSE) != null) {
             respondWith(messageContext, targetEPR);
         } else {
+            final MessageContext responseMessageContext = createResponseMessageContext(messageContext);
 
+            properties = BaseUtils.getEPRProperties(targetEPR);
+            final Handler<Exception> exceptionHandler = new Handler<Exception>() {
+                @Override
+                public void handle(Exception e) {
+                    log.error("Failed to send axis2 message", e);
+                    invokeSenderErrorCallback(messageContext, e);
+                }
+            };
             final URL url;
             try {
                 url = new URL(targetEPR);
             } catch (MalformedURLException e) {
                 throw AxisFault.makeFault(e);
             }
+            final AxisConfiguration axisConfiguration = messageContext.getConfigurationContext().getAxisConfiguration();
+            final TransportOutDescription transportOut = axisConfiguration.getTransportOut(url.getProtocol());
+            //use the protocol to load the http client. The host is set in axis2 and won't change. We'll use the
+            //path from the url though.
+            VertxConnectionFactory vertxConnectionFactory = new VertxConnectionFactory(transportOut);
+            final HttpClient httpClient = vertxConnectionFactory.getHttpClient(url);
+            final String protocalString = url.getProtocol();
+            log.debug("[Vertx Axis2 Transport] [Request] [Protocol] " + protocalString);
+            if(protocalString.equalsIgnoreCase("https")) {
+                httpClient.setSSL(true);
+            }
+            //we'll want to cache these clients
 
 
+            final String path = url.getPath();
+            HttpClientRequest httpClientRequest = httpClient.post(path, new Handler<HttpClientResponse>() {
+                @Override
+                public void handle(final HttpClientResponse httpClientResponse) {
+                    httpClientResponse.exceptionHandler(exceptionHandler);
+                    httpClientResponse.endHandler(new Handler<Void>() {
+                        @Override
+                        public void handle(final Void event) {
+                            log.debug("[Vertx Axis2 Transport] [END HANDLER]" + event.toString());
+                        }
+                    });
+                    httpClientResponse.bodyHandler(new Handler<Buffer>() {
+                        @Override
+                        public void handle(Buffer buffer) {
+                            log.debug("[Vertx Axis2 Transport] [Response] Body: {}", buffer.toString());
+                            for (Map.Entry<String, String> entry : httpClientResponse.headers().entrySet()) {
+                                log.debug("[Vertx Axis2 Transport] [Response] Header: {} {}", entry.getKey(), entry.getValue());
 
+                            }
 
-            //We tried to keep this on the main vertx thread, but that's not really possible now without forking axis2. We'll just switch threads to
-            //make sure the actual send is from a vertx thread
-            VertxLocator.vertx.runOnLoop(new SendHandler(messageContext, url));
+                            final String contentType = httpClientResponse.headers().get("content-type");
+                            final String soapAction = httpClientResponse.headers().get(BaseConstants.SOAPACTION.toLowerCase());
+                            try {
+                                VertxUtils.setSOAPEnvelope(buffer, responseMessageContext, contentType);
+                                final MessageReceiver messageReceiver = messageContext.getAxisOperation().getMessageReceiver();
+                                responseMessageContext.addRelatesTo(new RelatesTo(messageContext.getMessageID()));
+                                messageReceiver.receive(responseMessageContext);
+                            } catch (Exception e) {
+                                log.error("Unable to process response for axis2 client", e);
+                                invokeSenderErrorCallback(messageContext, e);
+                            }
+                            //invokeSenderCallback(messageContext, responseMessageContext);
+                        }
+                    });
+                }
+            });
+//            final String contentType = getContentType(messageContext);
+            final String contentType = "text/xml;charset=UTF-8"; //Not sure the right way to infer this in axis2 yet. Hard code for now.
+            httpClientRequest.exceptionHandler(exceptionHandler);
+            httpClientRequest.setTimeout(60000);
+
+            final Buffer buffer = getBodyBuffer(messageContext);
+            String soapAction = String.format("\"%s\"", messageContext.getSoapAction() != null ? messageContext.getSoapAction() : "");
+            log.info(String.format("[Vertx Axis2 Transport] [Request] [%s - %s]. Warning, contentType is hard coded right now !!!", path, contentType));
+            log.debug(String.format("[Vertx Axis2 Transport] [Request] [%s] - Request body: %s", path, buffer.toString()));
+            log.debug("[Vertx Axis2 Transport] [Request] SOAPAction: {}", soapAction);
+            log.debug("[Vertx Axis2 Transport] [Request] [BUFFER]: {}", buffer.toString());
+            
+            httpClientRequest = httpClientRequest.putHeader("Content-Type", contentType)
+                    .putHeader("Content-Length", buffer.length())
+                    .putHeader("SOAPAction", soapAction)
+                    .putHeader("User-Agent", "Apache-HttpClient/4.1.1 (java 1.5)");
+
+            if (buffer.length() == 0) {
+                httpClientRequest.end();
+            } else {
+                httpClientRequest.end(buffer);
+            }
         }
+
+
     }
 
     private void invokeSenderErrorCallback(MessageContext messageContext, Exception e) {
@@ -144,83 +220,5 @@ public class VertxSender extends AbstractTransportSender {
         }
 
         return sw.toString().getBytes();
-    }
-
-    private class SendHandler implements Handler<Void> {
-        private final MessageContext messageContext;
-        private final URL url;
-
-        public SendHandler(MessageContext messageContext, URL url) {
-            this.messageContext = messageContext;
-            this.url = url;
-        }
-
-        @Override
-        public void handle(Void event) {
-            final MessageContext responseMessageContext = createResponseMessageContext(messageContext);
-
-            final Handler<Exception> exceptionHandler = new Handler<Exception>() {
-                @Override
-                public void handle(Exception e) {
-                    log.error("Failed to send axis2 message", e);
-                    invokeSenderErrorCallback(messageContext, e);
-                }
-            };
-
-            HttpClient httpClient = (HttpClient) messageContext.getProperty(VertxConstants.HTTP_CLIENT);
-
-            final String path = url.getPath();
-            HttpClientRequest httpClientRequest = httpClient.post(path, new Handler<HttpClientResponse>() {
-                @Override
-                public void handle(final HttpClientResponse httpClientResponse) {
-                    httpClientResponse.exceptionHandler(exceptionHandler);
-                    httpClientResponse.bodyHandler(new Handler<Buffer>() {
-                        @Override
-                        public void handle(Buffer buffer) {
-                            log.debug("[Vertx Axis2 Transport] [Response] Body: {}", buffer.toString());
-                            for (Map.Entry<String, String> entry : httpClientResponse.headers().entrySet()) {
-                                log.debug("[Vertx Axis2 Transport] [Response] Header: {} {}", entry.getKey(), entry.getValue());
-
-                            }
-
-                            final String contentType = httpClientResponse.headers().get("content-type");
-                            final String soapAction = httpClientResponse.headers().get(BaseConstants.SOAPACTION.toLowerCase());
-                            try {
-                                VertxUtils.setSOAPEnvelope(buffer, responseMessageContext, contentType);
-                                final MessageReceiver messageReceiver = messageContext.getAxisOperation().getMessageReceiver();
-                                responseMessageContext.addRelatesTo(new RelatesTo(messageContext.getMessageID()));
-                                messageReceiver.receive(responseMessageContext);
-                            } catch (Exception e) {
-                                log.error("Unable to process response for axis2 client", e);
-                                invokeSenderErrorCallback(messageContext, e);
-                            }
-                            //invokeSenderCallback(messageContext, responseMessageContext);
-                        }
-                    });
-                }
-            });
-//            final String contentType = getContentType(messageContext);
-            final String contentType = "text/xml;charset=UTF-8"; //Not sure the right way to infer this in axis2 yet. Hard code for now.
-            httpClientRequest.exceptionHandler(exceptionHandler);
-            final Buffer buffer = getBodyBuffer(messageContext);
-            String soapAction = String.format("\"%s\"", messageContext.getSoapAction() != null ? messageContext.getSoapAction() : "");
-            log.info(String.format("[Vertx Axis2 Transport] [Request] [%s - %s]. Warning, contentType is hard coded right now !!!", path, contentType));
-            log.debug(String.format("[Vertx Axis2 Transport] [Request] [%s] - Request body: %s", path, buffer.toString()));
-            log.debug("[Vertx Axis2 Transport] [Request] SOAPAction: {}", soapAction);
-
-            httpClientRequest = httpClientRequest.putHeader("Content-Type", contentType)
-                    .putHeader("Content-Length", buffer.length())
-                    .putHeader("SOAPAction", soapAction);
-
-            if (buffer.length() == 0) {
-                httpClientRequest.end();
-            } else {
-                httpClientRequest.end(buffer);
-            }
-        }
-
-
-
-
     }
 }
